3f3ad75eb66b4cba9ab1f7124318734a577c8e30
kipkirui88 <hezekiahkoech@gmail.com>
2023-03-17 05:47:01 -0700

    task 2 to task 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *Return: always return 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

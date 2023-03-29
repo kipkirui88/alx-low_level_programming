bbbadcc39accc2a760065571654bdf3331a16e32
kipkirui88 <hezekiahkoech@gmail.com>
2023-03-28 19:31:53 -0700

    file 3to9

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * rev_string - reverse a string.
+  * @s: string to reverse
+  */
+
+void rev_string(char *s)
+{
+	int start, temp;
+	int end = 0;
+
+	while (*(s + end) != '\0')
+	{
+		end++;
+	}
+
+	end -= 1;
+	start = 0;
+	while (end > start)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+
+}
+
